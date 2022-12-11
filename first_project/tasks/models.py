e82d889a4687c2c60e91bdca078f63d1cfbf44c6
Tigran Danielyan <tigran.danielyan3@gmail.com>
2022-12-11 22:51:50 +0400

    added

--- first_project/tasks/models.py
+++ first_project/tasks/models.py
@@ -8,20 +8,21 @@ STATUS_CHOICES = (
 )
 
 
-class Task(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    description = models.TextField(null=True, blank=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
-    created_at = models.DateTimeField(auto_now=True)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.name}-{self.status}"
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(null=True, blank=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    created_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="task_set")
+
+    def __str__(self):
+        return f"{self.name}-{self.status}"
+
